c202983785dd6d58683bf1bee04ec311a9e47035
Dickson <1023524@student.sae.edu.au>
2021-06-18 01:03:45 +0800

    Edited Main Scripts

--- Assets/Scripts/MainScripts/MicrogameManager.cs
+++ Assets/Scripts/MainScripts/MicrogameManager.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum MicrogameState
+{
+	DISCLAIMER,
+	PREMURDER,
+	MURDER,
+	CLEANUP,
+	CHASE,
+	CAUGHT,
+	JAIL,
+	END
+}
+
 public class MicrogameManager : MonoBehaviour
 {
-	public static MicrogameManager current;
+	[SerializeField]
+	private string[] premurderMicrogames, meleeMurderMicrogames, gunMurderMicrogames, cleanupMicrogames, chaseMicrogames, caughtMicrogames, jailMicrogames;
 
 	[SerializeField]
-	private BaseMicrogameClass[] startMicrogamesArray;
+	private string startingMicrogame;
 	private static string currentMicrogameName;
 
+	public static MicrogameManager current;
+
 	private bool FirstMicrogame = true;
+	private MicrogameState currentState;
 
 	private void Awake()
 	{
@@ -20,17 +36,63 @@ public class MicrogameManager : MonoBehaviour
 
 	public void StartMicrogames()
 	{
-		if (startMicrogamesArray != null && FirstMicrogame)
+		if (startingMicrogame != null && FirstMicrogame)
 		{
 			FirstMicrogame = false;
-			currentMicrogameName = startMicrogamesArray[Random.Range(0, startMicrogamesArray.Length)].GetCurrentSceneName();
+			currentState = MicrogameState.DISCLAIMER;
+			currentMicrogameName = startingMicrogame;
 			SceneManager.LoadScene(currentMicrogameName);
 		}
 	}
 
-	public static void RandomizeMicrogame(BaseMicrogameClass microgameClass)
+	public void LoadNextMicrogame()
 	{
-		currentMicrogameName = microgameClass.GetNextAvailableRandomScene();
-		SceneManager.LoadScene(currentMicrogameName);
+		string nextMicrogame = "";
+		switch (currentState)
+		{
+			case MicrogameState.DISCLAIMER:
+				currentState = MicrogameState.PREMURDER;
+				nextMicrogame = RandomiseMicrogame(premurderMicrogames);
+				break;
+			case MicrogameState.PREMURDER:
+				currentState = MicrogameState.MURDER;
+				nextMicrogame = RandomiseMicrogame(gunMurderMicrogames);
+				break;
+			case MicrogameState.MURDER:
+				currentState = MicrogameState.CLEANUP;
+				nextMicrogame = RandomiseMicrogame(cleanupMicrogames);
+				break;
+			case MicrogameState.CLEANUP:
+				currentState = MicrogameState.CHASE;
+				nextMicrogame = RandomiseMicrogame(chaseMicrogames);
+				break;
+			case MicrogameState.CHASE:
+				currentState = MicrogameState.CAUGHT;
+				nextMicrogame = RandomiseMicrogame(caughtMicrogames);
+				break;
+			case MicrogameState.CAUGHT:
+				currentState = MicrogameState.JAIL;
+				nextMicrogame = RandomiseMicrogame(jailMicrogames);
+				break;
+			case MicrogameState.JAIL:
+				currentState = MicrogameState.END;
+				break;
+		}
+		SceneManager.LoadScene(nextMicrogame);
 	}
+
+	private string RandomiseMicrogame(string[] microgameScenes)
+	{
+		return microgameScenes[Random.Range(0,microgameScenes.Length)];
+	}
+
+	//public MicrogameState GetCurrentMicrogameState()
+	//{
+	//	return currentState;
+	//}
+
+	//public void SetCurrentMicrogameState(MicrogameState state_param)
+	//{
+	//	currentState = state_param;
+	//}
 }
