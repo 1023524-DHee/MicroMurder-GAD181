1da805bf7c999e4debc913f842eeb234191e293c
Dickson <1023524@student.sae.edu.au>
2021-06-25 12:05:02 +0800

    Updated Lying_MG Scene

--- Assets/Scripts/MicrogameScripts/Lying_MG/BPMTracker.cs
+++ Assets/Scripts/MicrogameScripts/Lying_MG/BPMTracker.cs
@@ -6,11 +6,12 @@ public class BPMTracker : MonoBehaviour
 {
     private bool canPress = false;
     private bool BPMChanged = false;
+    private bool pressed = false;
 
     public float currentBPM;
+    public float amountToChange;
     public GameObject heartSprite;
     public GameObject circleSprite;
-    public float amountToChange;
 
     // Start is called before the first frame update
     void Start()
@@ -20,53 +21,76 @@ public class BPMTracker : MonoBehaviour
 
 	private void Update()
 	{
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
+            pressed = true;
             if (canPress)
             {
-                print("PRESSED");
+                HealthBar.current.TakeDamage(false);
+            }
+            else
+            {
+                HealthBar.current.TakeDamage(true);
             }
         }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            BPMChanged = true;
+            SetBPMValue(amountToChange);
         }
 
     }
 
+    // Shrinks the timing circle
     IEnumerator ShrinkCircle()
     {
         float startTime = Time.time;
         float endTime = startTime + currentBPM;
         Vector3 initCircleScale = circleSprite.transform.localScale;
+
         while(Time.time < endTime)
         {
             circleSprite.transform.localScale = Vector3.Lerp(initCircleScale, new Vector3(0.5f,0.5f,0.5f), (Time.time-startTime)/currentBPM);
-
             if (circleSprite.transform.localScale.x < 1.3f)
             {
                 canPress = true;
             }
-
             yield return null;
         }
         canPress = false;
         circleSprite.transform.localScale = initCircleScale;
-        HandleBPMChange(amountToChange);
+
+        if (!pressed)
+        {
+            HealthBar.current.TakeDamage(true);
+        }
+        pressed = false;
+        HandleBPMChange();
     }
 
-    private void HandleBPMChange(float changedBPMValue)
+    // Handles BPM change at runtime
+    private void HandleBPMChange()
     {
         if (BPMChanged)
         {
-            currentBPM = changedBPMValue;
+            currentBPM = amountToChange;
             BPMChanged = false;
         }
         StartCoroutine(ShrinkCircle());
     }
 
+    // Set the new BPM value
     public void SetBPMValue(float newBPMValue)
     {
+        BPMChanged = true;
         amountToChange = newBPMValue;
+    }
+
+    public void ClickDetected()
+    {
+        if (canPress)
+        {
+            HealthBar.current.TakeDamage(false);
+        }
     }
 }
