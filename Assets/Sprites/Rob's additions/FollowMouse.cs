81bf483e4f05853b31411b033655293342a6cd16
Robert Craig <1023817@student.sae.edu.au>
2021-07-08 11:02:38 +0800

    Sniper shooting cont. 1

--- Assets/Sprites/Rob's additions/FollowMouse.cs
+++ Assets/Sprites/Rob's additions/FollowMouse.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
-    public void Update()
+    void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+
+    void Update()
     {
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(cursorPos.x, cursorPos.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Cursor.visible = true;
+        }
     }
 }
 
