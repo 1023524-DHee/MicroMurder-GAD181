fa020b1950d0f76e4f7021bb32500d98bfd90f4a
Rhys <rhysvc@iinet.net.au>
2021-07-09 17:25:27 +0800

    Finished FollowingSomeone_MG (Except Audio)
    Created victim assets from player assets. Scripted and animated victim movements and trigger collider to reset level on enter. Microgame scene transition set up.

--- Assets/Scripts/MicrogameScripts/Following_MG/VictimController.cs
+++ Assets/Scripts/MicrogameScripts/Following_MG/VictimController.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictimController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2[] points;
+    public int pointNumber = 0;
+    private Vector3 currentTarget;
+
+    public float tolerance;
+    public float speed; // how fast the victim moves
+    public float delayTimer; // how long the victim waits after stopping
+
+    private float delayStart;
+    private bool delayed;
+
+    // should the platform move on its own
+    public bool automatic;
+
+    public Animator animator;
+
     void Start()
     {
-        
+        if (points.Length > 0)
+        {
+            currentTarget = points[0];
+        }
+        tolerance = speed * Time.deltaTime;
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position != currentTarget)
+        {
+            MovePosition();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+    }
+
     void Update()
     {
-        
+        animator.SetBool("Delayed", delayed);
+    }
+
+    // moves to the next position
+    void MovePosition()
+    {
+        Vector3 heading = currentTarget - transform.position;
+        transform.position += (heading / heading.magnitude) * speed * Time.deltaTime;
+        if (heading.magnitude < tolerance)
+        {
+            transform.position = currentTarget;
+            delayed = true;
+            delayStart = Time.time;
+        }
+    }
+
+    // automatically moves to next platform
+    void UpdateTarget()
+    {
+        if (automatic)
+        {
+            if (Time.time - delayStart > delayTimer)
+            {
+                delayed = false;
+                NextPosition();
+            }
+        }
+    }
+
+    public void NextPosition()
+    {
+        pointNumber++;
+        if (pointNumber >= points.Length)
+        {
+            pointNumber = 0;
+        }
+        currentTarget = points[pointNumber];
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            print("Caught");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
