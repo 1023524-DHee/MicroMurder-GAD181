6fce0578e339cd780e578ca78ae31cbe4729c35b
Robert <Craig>
2021-07-14 12:07:41 +0800

    Sniper shooting cont 4

---
+++ Assets/Sprites/Rob's additions/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+
+    private Vector2 direction;
+
+    void Start()
+    {
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction = direction.normalized;
+    }
+
+    void Update()
+    {
+        transform.position = direction * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Equals("Player") && !collision.name.Equals("Bullet(Clone)"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
