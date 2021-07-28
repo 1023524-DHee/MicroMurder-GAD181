58539a6dd1e5bab45551f9150c0e6306aae024c0
Robert <Craig>
2021-07-28 19:13:33 +0800

    Clearing up files

--- Assets/Scripts/MicrogameScripts/ShootingSniper_MG/DestroyMyself.cs
+++ Assets/Scripts/MicrogameScripts/ShootingSniper_MG/DestroyMyself.cs
@@ -7,17 +7,20 @@ public class DestroyMyself : MonoBehaviour
  
     public static DestroyMyself current;
     public void DestroyFunction(GameObject objectToDestroy)
+
     {
+        Cursor.visible = true;
         GetComponent<AudioSource>().Play();
         GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
         playerGO.SetActive(false);
         MicrogameManager.current.LoadNextMicrogame();
+
     }
 
     void Start()
     {
         
         current = this;
-       
+        
     }
 }
